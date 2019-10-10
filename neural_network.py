245c7e1aa2829e0cc00aa607996b58a0366073e8
Edgar Gonzalez <edgar.gonzalez2@mavs.uta.edu>
2019-10-10 13:38:24 -0700

    Final

--- neural_network.py
+++ neural_network.py
@@ -1,126 +1,146 @@
-import numpy as np
-import pandas
 import sys
-import operator
 import math
-class NN:
-	def __init__(self, training_path, test_path, layer_count, units_per_layer, epochs):
-
-		self.classes_train = {}
-		self.idx_mapping = {}
-		self.count = 0
-		## Network hyper paramaters 
-		self.layers = int(layer_count)
-		self.units_per_layer = int(units_per_layer)
-		self.epochs = int(epochs)
-		self.U = self.units_per_layer * self.layers
-
-
-		# Variables related to the data 
-		self.input_shape = None
-		self.output_shape = None
-		self.load_classes(training_path, True)
-		self.load_classes(test_path, False)
-		self.training_path = training_path
-		self.test_path = test_path
-		self.train_labels = None
-		self.test_labels = None
-		self.test_data = None
-
-
-		## Network architecture varaibles
-		self.weights = self.create_weights()
-	def sigmoid(self, x):
-		return 1 / (1 + math.exp(-x))
-	def sigmoid_der(self, z, expected):
-		return (z - expected) * z * (1 - z)
-	def load_data(self, data_path):
-		return np.loadtxt(data_path)
-	def load_classes(self, data_path, is_training):
-		data = self.load_data(data_path)
-		labels = self.make_one_hot(data[ :, [-1]])
-		if is_training:
-			for l, row in enumerate(data):
-				self.count += 1
-				class_label = tuple(labels[l])
-				if class_label not in self.classes_train:
-					self.classes_train[class_label] = np.hstack((1,row[:-1])), 
-				else:
-					self.classes_train[class_label] = np.vstack([self.classes_train[class_label][0], np.hstack((1,row[:-1]))])
-			
-			self.input_shape = self.classes_train[class_label].shape[1]
-			self.D = self.classes_train[class_label].shape[1] 
-			self.U += self.classes_train[class_label].shape[1]
-			self.classes_train = dict(sorted(self.classes_train.items()))
-			for i,x in enumerate(self.classes_train):
-				self.idx_mapping[i] = x
-			self.output_shape = len(self.idx_mapping.keys())
-			self.U += self.output_shape
-		else:
-			ones_arr = np.ones((data.shape[0], 1))
-			self.test_labels = labels
-			self.test_data = data[:, [x for x in range(data.shape[1] - 1)]]
-			self.test_data = np.hstack((ones_arr, self.test_data))
-		data = self.load_data(data_path)
-	def create_weights(self):
-		weights = []
-		if self.layers > 1:
-			weights.append(np.random.uniform(-0.5, 0.5, (self.input_shape) * (self.units_per_layer + 1)))
-			for i in range(self.layers):
-				weights.append(np.random.uniform(-0.5, 0.5, (self.units_per_layer + 1) ** 2))
-			weights.append(np.random.uniform(-0.5, 0.5, (self.units_per_layer + 1) * self.output_shape))
-		else:
-			weights.append(np.random.uniform(-0.5, 0.5, self.input_shape * self.output_shape))
-		return weights
-	def create_ZA(self, x):
-		Z = np.zeros(self.U)
-		A = np.zeros(self.U)
-		for i in range(self.D):
-			Z[i] = x[i]
-		return Z,A
-	def make_one_hot(self, labels):
-		col_max = np.asscalar(np.max(labels))
-		ones_arr = np.zeros((labels.shape[0], int(col_max)))
-		for i,x in enumerate(labels):
-			ones_arr[i][max(0, int(x) - 1)] = 1
-		return ones_arr
-	def feed_forward(self, x):
-		## Not sure if this weights update will work
-		Z, A = self.create_ZA(x)
-		for x in self.weights:
-			print(x, x.shape)
-			print()
-		for i in range(self.layers):
-			break
-		return Z,A
-	def backprop(self, Z , A, t):
-		idx = self.D + (self.units_per_layer * self.layers)
-		Delta = np.zeros(self.U)
-		# for i, x in enumerate(range(idx, len(Z))):
-		# 	print(Z[x], t[i], self.sigmoid_der(Z[x], t[i]))
-	def train(self):
-		for i in self.classes_train:
-			for x in self.classes_train[i]:
-				Z,A = self.feed_forward(x)
-				self.backprop(Z,A, i)
-				break
-			break
-	def predict(self, x):
-		pass
-	def get_one_hot(self, x, class_label, count, avg):
-		pass
-	def print_meta(self):
-		print(f'Layers: {self.layers}')
-		print(f'Units: {self.units_per_layer}')
-		print(f'Epochs: {self.epochs}')
-		print(f'Input Shape: {self.input_shape}')
-		print(f'Output Shape: {self.output_shape}')
+import numpy as np 
+from scipy.stats import truncnorm
+def get_truncated_normal(mean=0, sd=1, low=-0.5, upp=0.5):
+    return truncnorm(
+        (low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
+def sigmoid(x):
+	return 1 / (1 + np.exp(-x))
+def sigmoid_prime(x):
+	return sigmoid(x) * (1 - sigmoid(x))
 def main():
 	if len(sys.argv) < 6:
 		print('Usage: [path to training file] [path to test file] layer_count units_per_layer rounds')
-	classifier = NN(*sys.argv[1:6])
+	classifier = NeuralNet(*sys.argv[1:6])
 	classifier.train()
-	classifier.print_meta()
+	classifier.run_predictions()
+
+
+class Layer:
+	def __init__(self, input_dim, output_dim):
+		self.lr = 1
+		self.input_dim = input_dim
+		self.output_dim = output_dim
+		self.weights = np.random.uniform(-0.5,0.5, (self.input_dim, self.output_dim))
+		self.biases = np.zeros((output_dim,1))
+	def feedforward(self, x):
+		return sigmoid(np.dot(self.weights.T, x))
+	def update_lr(self):
+		self.lr *= .98
+
+
+class NeuralNet:
+	def __init__(self, train_path, test_path, layers, units_per_layer, epochs):
+		self.layer_count = int(layers)
+		self.units = int(units_per_layer)
+		self.epochs = int(epochs)
+
+		self.mapping = {}
+
+		self.training_data = self.load(train_path, True) / self.training_max
+		self.test_data = self.load(test_path, False) / self.training_max
+
+
+		self.network = self.load_network()
+	def get_class(self, vec):
+		return np.argmax(vec, axis=0)
+	def load_network(self):
+		net = []
+		if self.layer_count == 0:
+			net.append(Layer(self.input_shape,self.output_shape))
+			return net
+		for x in range(self.layer_count):
+			if x == 0:
+				net.append(Layer(self.input_shape, self.units))
+				continue
+			net.append(Layer(self.units, self.units))
+		net.append(Layer(self.units, self.output_shape))
+		return net
+	def load(self, path, is_training):
+		data = np.loadtxt(path)
+		if is_training:
+			self.make_one_hot(data[: , [-1]], True)
+			self.training_max = np.amax(data)
+		else:
+			self.make_one_hot(data[:, [-1]], False)
+		self.input_shape = data.shape[1] - 1
+		self.output_shape = self.training_labels.shape[1]
+		return data[: , [x for x in range(data.shape[1] - 1 )]]
+	def make_one_hot(self, labels, is_training):
+		labels_set = set()
+		for x in labels:
+			labels_set.add(int(np.asscalar(x)))
+		labels_set = sorted(labels_set)
+		for i,x in enumerate(labels_set):
+			self.mapping[x] = i
+		zeros_arr = np.zeros((labels.shape[0],len(labels_set)))
+		for i,y in enumerate(labels):
+			number = int(np.asscalar(y))
+			idx = self.mapping[number]
+			zeros_arr[i][idx] = 1
+		if is_training:
+			self.training_labels = zeros_arr
+		else:
+			self.test_labels = zeros_arr
+	def feedforward(self, x):
+		input_ = x
+		Z = []
+		if self.layer_count == 0:
+			return self.network[0].feedforward(input_)
+		else:
+			for i,y in enumerate(self.network):
+				Z.append(y.feedforward(input_))
+				input_ = Z[-1]
+		return Z
+	def backprop(self, Z, label):
+		if self.layer_count == 0:
+			pass
+		elif self.layer_count == 1:
+			pass
+		else:
+			pass		
+			# hm = {}
+			# output_layer = Z[-1]
+			# error = output_layer - label.reshape(len(label),1)
+			# sigmoid_term = sigmoid_prime(Z[-1])
+			# delta = error * sigmoid_term
+			# dw = np.dot(Z[-2],delta.T)
+
+			# hm[len(Z) - 1] = dw,delta
+			# for y in reversed(range(2, len(self.network))):
+			# 	delta = np.dot(self.network[y].weights, delta) * sigmoid_prime(Z[y-1]) * Z[y-1]
+			# 	# dw = np.dot(Z[y-1],delta.T)
+			# 	hm[y-1] = delta,dw
+			# for k,v in hm.items():
+			# 	self.network[k].weights = self.network[k].weights - (self.network[k].lr * v[0])
+			# 	self.network[k].update_lr()
+			# 	# self.network[k].biases -= self.network[k].lr * np.mean(v[0], 0)
+	def train(self):
+		iterations = 0
+		for x in range(self.epochs):
+			for i,y in enumerate(self.training_data):
+				Z = self.feedforward(y.reshape(len(y),1))
+				self.backprop(Z, self.training_labels[i])
+	def run_predictions(self):
+		count = 1
+		avg = 0
+		for i,y in enumerate(self.test_data):
+			prediction = self.feedforward(y.reshape(len(y),1))[-1]
+			predicted = self.get_class(prediction)
+			true = self.get_class(self.test_labels[i])
+			accuracy = 1 if predicted == true else 0
+			print("ID=%5d, predicted=%3d, true=%3d, accuracy=%4.2f\n" % (count, predicted, true, accuracy))
+			count += 1
+			avg += accuracy
+		print("classification accuracy=%6.4f" % (avg / (count-1)))
+	def single_step():
+		x = self.test_data[0]
+		x_label = self.test_labels[0]
+		Z = self.feedforward(x)
+		print('Z:', Z)
+		print()
+		print('Output:', Z[-1])
+		print()
 if __name__ == '__main__':
-	np.set_printoptions(threshold=sys.maxsize)
 	main()
